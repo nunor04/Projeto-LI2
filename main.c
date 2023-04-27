2005d1f31ae1b1f7cdcdae7442979cbd048952aa
a104258 <a104258@alunos.uminho.pt>
2023-04-27 11:38:58 +0100

    rudimentos da geração do mapa visivel, muitoserros

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <ncurses.h>
 #include <time.h>
+#include <math.h>
 
 #include "mapa.h"
 
@@ -16,9 +17,11 @@
  * - o que está em cada casa
  *
  */
-typedef struct state {
+typedef struct state
+{
 	int playerX;
 	int playerY;
+	int playerHP;	//o que vai contar a hp do jogador, podera ser usado depois para o sistema de vida
 } STATE;
 
 
@@ -26,52 +29,85 @@ typedef struct state {
  *
  * Um pequeno exemplo que mostra o que se pode fazer
  */
-void do_movement_action(STATE *st, int dx, int dy) {
+void do_movement_action(STATE *st, int dx, int dy)
+{
 	st->playerX += dx;
 	st->playerY += dy;
 }
 
-void drawplayer();
+void drawLight(STATE *st, int mapData[][70], int nrows, int ncols)
+{
+		int i, j;
+	for (i = 0; i < nrows; i++)
+	{
+		for (j = 0; j < ncols; j++)
+		{
+			if(sqrt(pow(i - st->playerX, 2) + pow(j - st->playerY, 2)) <= 5)
+			{
+				if(mapData[i][j] == 0)
+					mvaddch(i, j, '.');
+				if(mapData[i][j] == 1)
+					mvaddch(i, j, '#');
+				if(mapData[i][j] == 2)
+					mvaddch(i, j, '~');
+			}
+		}
+	}
+}
 
-void update(STATE *st) {
+void update(STATE *st)
+{
 	int key = getch();
 
 	mvaddch(st->playerX, st->playerY, '.');
 	switch(key) {
 		case KEY_A1:
-		case '7': do_movement_action(st, -1, -1); break;
+			case '7': do_movement_action(st, -1, -1);
+				break;
 		case KEY_UP:
-		case '8': do_movement_action(st, -1, +0); break;
+			case '8': do_movement_action(st, -1, +0);
+				break;
 		case KEY_A3:
-		case '9': do_movement_action(st, -1, +1); break;
+			case '9': do_movement_action(st, -1, +1);
+				break;
 		case KEY_LEFT:
-		case '4': do_movement_action(st, +0, -1); break;
+			case '4': do_movement_action(st, +0, -1);
+				break;
 		case KEY_B2:
-		case '5': break;
+			case '5':
+				break;
 		case KEY_RIGHT:
-		case '6': do_movement_action(st, +0, +1); break;
+			case '6': do_movement_action(st, +0, +1);
+				break;
 		case KEY_C1:
-		case '1': do_movement_action(st, +1, -1); break;
+			case '1': do_movement_action(st, +1, -1);
+				break;
 		case KEY_DOWN:
-		case '2': do_movement_action(st, +1, +0); break;
+			case '2': do_movement_action(st, +1, +0);
+				break;
 		case KEY_C3:
-		case '3': do_movement_action(st, +1, +1); break;
-		case 'q': endwin(); exit(0); break;
+			case '3': do_movement_action(st, +1, +1);
+				break;
+		case 'q': endwin(); 
+			exit(0);
+			break;
 	}
 }
 
 int main() {
-	STATE st = {20,20};
+	STATE st = {20,20,3};	//coordenada 20,20, começa com 3HP
 	WINDOW *wnd = initscr();
 	int ncols, nrows;
 	getmaxyx(wnd,nrows,ncols);
+
+	int mapData[nrows][ncols];
 
 	srandom(time(NULL));
 	start_color();
 
 	cbreak();
 	noecho();
-	nonl();
+	nonl();	
 	intrflush(stdscr, false);
 	keypad(stdscr, true);
 
@@ -79,7 +115,13 @@ int main() {
         init_pair(COLOR_YELLOW, COLOR_YELLOW, COLOR_BLACK);
         init_pair(COLOR_BLUE, COLOR_BLUE, COLOR_BLACK);
 
-	gerar();
+	/* 	int i, j;
+	for(i = 0; i < nrows; i++)
+	{
+		for(j = 0; j < ncols; j++)
+        	mapData[i][j] = 0;
+    } */												//isto e so teste para preencher o array
+	gerar(mapData);
 
 	/**
 	 * Este código está muito mal escrito!
@@ -95,8 +137,9 @@ int main() {
 	while(1) {
 		move(nrows - 1, 0);
 		attron(COLOR_PAIR(COLOR_BLUE));
-		printw("(%d, %d) %d %d", st.playerX, st.playerY, ncols, nrows);
+		printw("(%d, %d) %d %d", st.playerX, st.playerY, ncols, nrows); //ao tirar isto da warning de ncols e nrows nao utilizadas
 		attroff(COLOR_PAIR(COLOR_BLUE));
+		drawLight(&st, mapData, nrows, ncols);
 		attron(COLOR_PAIR(COLOR_YELLOW));
 		mvaddch(st.playerX, st.playerY, '@' | A_BOLD);
 		attroff(COLOR_PAIR(COLOR_YELLOW));
@@ -109,7 +152,7 @@ int main() {
 		// mvaddch(st.playerX + 1, st.playerY - 1, '.' | A_BOLD);
 		// mvaddch(st.playerX + 1, st.playerY + 0, '.' | A_BOLD);
 		// mvaddch(st.playerX + 1, st.playerY + 1, '.' | A_BOLD);
-                attroff(COLOR_PAIR(COLOR_YELLOW));
+        attroff(COLOR_PAIR(COLOR_YELLOW));
 		move(st.playerX, st.playerY);
 		update(&st);
 	}
--- mapa.c
+++ mapa.c
@@ -5,70 +5,65 @@
 #include <time.h>
 
 
-void gerar()
+/* 
+codigo de matriz para armazenar as informações do mapa
+    0 -> espaço em branco '.';
+    1 -> parede '#';
+    2 -> espaço de dano ambiental (lava, veneno,...) '~';
+
+    3 -> espaço de dano do jogador '+';
+    4 -> espaço de dano do inimigo (sem display).
+*/
+
+void gerar(int mapData[][70])
 {
-    int row, col;
-    int max_row, max_col;
-    int count = 0;
+        int row, col,
+            max_row, max_col,
+            xwall_size, ywall_size, start_row, start_col,
+            i, j,
+            count = 0;
     //srand(time(NULL));
 
     // Obtém o número máximo de linhas e colunas
     getmaxyx(stdscr, max_row, max_col);
 
-    // Preenche o ecrã com o caractere '.'
-    // for (row = 0; row < max_row; row++) {
-    //     if(row==0 || row==1 || row==max_row-1 || row==max_row-2){
-    //         for(col = 0; col < max_col; col++){
-    //         mvaddch(row,col,'#');
-    //         }
-    //     }
-    //     else{
-    //         for (col = 0; col < max_col; col++) {
-    //             if(col==0 || col==1 || col==max_col-1 || col==max_col-2){
-    //                 for(row = 0; row < max_row; row++){
-    //                 mvaddch(row,col,'#');
-    //                 }
-    //             }
-    //             else{
-    //                 for(row = 2; row < max_row; row++){
-    //                     mvaddch(row, col, '.');
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-
-    for(row = 0; row < max_row; row++)
+    for (row = 0; row < max_row; row++)
     {
-        for(col = 0; col < max_col; col++)
-            mvaddch(row,col,'#');
+        for (col = 0; col < max_col; col++)
+            mapData[row][col] = 1;
     }
+        //preenche a tela toda com '1'
 
-    for(row = 2; row < max_row-2; row++)
+    for (row = 2; row < max_row-2; row++)
     {
-        for(col = 2; col < max_col-2; col++)
-            mvaddch(row,col,'.');
+        for (col = 2; col < max_col-2; col++)
+            mapData[row][col] = 0;
     }
+        //preenche a tela toda, exceto as duas linhas de fora da tela com '0' para ter o efeito desejado das bordas
 
     // Coloca o caractere '#' aleatoriamente em grupos
-    while (count < max_row * max_col * 0.4) { // 40% de ocupação
-        int group_size = rand() % 4 + 4; // tamanho aleatório do grupo
-        int start_row = rand() % max_row;
-        int start_col = rand() % max_col;
-        int i, j;
+    while (count < max_row * max_col * 0.4)                          // 40% de ocupação (calcula a area do ecra e depois pega em 40% disso)
+    {
+        xwall_size = rand() % 4 + 4;
+        ywall_size = rand() % 4 + 4; 
+            // tamanho aleatório do grupo, com o segundo elemento funcionando como tipo "zoom"
+        start_row = rand() % max_row;
+        start_col = rand() % max_col;
+            // os mods(%) garantem que startrow e col cabem no ecrã porque se for o mod entao será sempre menor que maxrow e col
 
         // Verifica se o grupo cabe no ecrã
-        if (start_row + group_size >= max_row || start_col + group_size >= max_col) {
+        if (start_row + xwall_size >= max_row && start_col + ywall_size >= max_col) {
             continue;
         }
 
-        // Preenche o grupo com o caractere '#'
-        for (i = start_row; i < start_row + group_size; i++) {
-            for (j = start_col; j < start_col + group_size; j++) {
-                mvaddch(i, j, '#');
-            }
+        // Preenche o grupo com o nr '1'
+        for (i = start_row; i < start_row + xwall_size; i++)
+        {
+            for (j = start_col; j < start_col + ywall_size; j++)
+                mapData[i][j] = 1;
         }
 
-        count += group_size * group_size;
+        count += xwall_size * ywall_size;
+            //tira a area preenchida pelo "bloco" dos espaços possiveis para preencher
     }
 }
